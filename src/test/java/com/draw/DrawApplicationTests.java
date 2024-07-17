2c78b68462dc6991c4d713c8ca8178f1cda361e3
taehyeonn <97taehyun@gmail.com>
2024-07-17 23:04:07 +0900

    initial commit

---
+++ src/test/java/com/draw/DrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
